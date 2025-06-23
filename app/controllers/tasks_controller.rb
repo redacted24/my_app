aba1ef2e2041d0b94b74846eb921861a4479df74
Rails <qu.digitalo>
2025-06-23 19:35:29 +0000

    Revert "batman 2"
    This reverts commit 431f8d87ebde8906e462295c57a9273f810e23bf.

--- app/controllers/tasks_controller.rb
+++
@@ -1,6 +0,0 @@
-class TasksController < ApplicationController
-
-    def show
-    end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "tasks#index"
-  resources :tasks
+  # root "posts#index"
 end
