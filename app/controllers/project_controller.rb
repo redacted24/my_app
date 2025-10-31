ec995609112600a0c5edac09ac8b18b1b6f42246
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2025-10-31 14:27:14 -0400

    add routes

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,5 +1,11 @@
 class ProjectController < ApplicationController
   def index
     @project = Project.all
+    @firstProject = Project.first
+  end
+
+  # GET /project/{#id} triggers the show method 
+  def show
+
   end
 end
