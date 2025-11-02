496f8e4ebd4cd760255ff0964863079c674ac52a
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2025-11-02 17:14:50 -0500

    added more items to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,60 @@
 #   end
 #
 
-# Poker playground
-Project.find_or_create_by!(
-  name: "Poker Playground",
-  url_string: "poker_playground",
-  tags: [ "Python", "Flask", "React" ],
-  github_link: "",
-  devpost_link: "",
-  website_link: "",
-  custom_link: "",
-  comments: "",
-  active: 0,
-  unreleased: 0,
-  description: "",
-  featured: 1,
-  date: "2025"
+# clean slate
+Project.delete_all
+ActiveStorage::Attachment.all.each { |attachment| attachment.purge }
+
+project = Project.new(
+    name: "Poker Playground",
+    url_string: "poker_playground",
+    tags: [ "Python", "Flask", "React" ],
+    github_link: "http://www.github.com/redacted24/poker",
+    devpost_link: "",
+    website_link: "",
+    custom_link: "",
+    comments: "",
+    active: 0,
+    unreleased: 0,
+    description: "Developed as part of a Capstone project at Marianopolis College.",
+    featured: 1,
+    date: "2024"
+)
+project.overview_image.attach(io: File.open("app/assets/images/poker_playground_preview.png"), filename: "pokerpreview.png")
+project.save
+
+
+Project.create!(
+    name: "Chat Analyzer",
+    url_string: "chat_analyzer",
+    tags: [ "Python", "Java" ],
+    github_link: "",
+    devpost_link: "",
+    website_link: "",
+    custom_link: "",
+    comments: "",
+    active: 0,
+    unreleased: 0,
+    description: "Developed a Windows desktop app that decrypts locally stored databases of a popular messaging service and
+monitors user chats for potentially sensitive topics",
+    featured: 1,
+    date: "2025"
+)
+
+Project.create!(
+    name: "Duel Vision",
+    url_string: "duel_vision",
+    tags: [ "React", "Websockets", "Mediapipe" ],
+    github_link: "Developed a Windows desktop app that decrypts locally stored databases of a popular messaging service and
+monitors user chats for potentially sensitive topics",
+    devpost_link: "",
+    website_link: "",
+    custom_link: "",
+    comments: "",
+    active: 0,
+    unreleased: 0,
+    description: "Integrated computer vision through MediaPipe to enable players to control and shoot spaceships with hand
+movements.",
+    featured: 1,
+    date: "2025"
 )
